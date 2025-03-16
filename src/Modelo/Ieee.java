148e6ac921d5166b4e5a5522d9cde2bab8265b8f
Angie Ortiz <angienikoloa@ufps.edu.co>
2025-03-16 13:30:01 -0500

    IEEE a Decimal y Viceversa

---
+++ src/Modelo/Ieee.java
@@ -0,0 +1,183 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author N
+ */
+public class Ieee {
+
+    String signo;
+    String expBinary;
+    String mantisa;
+
+    public Ieee() {
+    }
+   
+
+    public Ieee(String signo, String expBinary, String mantisa) {
+        this.signo = signo;
+        this.expBinary = expBinary;
+        this.mantisa = mantisa;
+    }
+
+    public String getSigno() {
+        return signo;
+    }
+
+    public String getExpBinary() {
+        return expBinary;
+    }
+
+    public String getMantisa() {
+        return mantisa;
+    }
+
+    public void setSigno(String signo) {
+        this.signo = signo;
+    }
+
+    public void setExpBinary(String expBinary) {
+        this.expBinary = expBinary;
+    }
+
+    public void setMantisa(String mantisa) {
+        this.mantisa = mantisa;
+    }
+
+    public String conversionIEEEde32Bits(double num) {
+
+        if (num == 0.0) {
+            return "00000000000000000000000000000000";
+        }
+
+        signo = (num < 0) ? "1" : "0";
+        num = Math.abs(num);
+
+        double temp = num;
+        int iterador = 0;
+        while (temp > 2) {
+            temp /= 2;
+            iterador++;
+        }
+        while (temp < 1) {
+            temp *= 2;
+            iterador--;
+        }
+
+        int exponente = 127 + iterador;
+        expBinary = String.format("%8s", decimalAbinario(exponente)).replace(' ', '0');
+
+        temp -= 1.0;
+        String binario = decimalAbinarioD(temp);
+
+        mantisa = binario.substring(2);
+        if (mantisa.length() > 23) {
+            mantisa = mantisa.substring(0, 23);
+        } else {
+            while (mantisa.length() < 23) {
+                mantisa += "0";
+            }
+        }
+        return signo + expBinary + mantisa;
+    }
+
+    private String decimalAbinario(int numero) {
+        String binario = "";
+        int modulo;
+        while (numero > 0) {
+            modulo = numero % 2;
+            binario = modulo + binario;
+            numero /= 2;
+        }
+        return binario;
+    }
+
+    private String decimalAbinarioD(double numero) {
+        int parteEntera = (int) numero;
+        String binarioEntero = Integer.toBinaryString(parteEntera);
+        double parteFraccionaria = numero - parteEntera;
+        StringBuilder binarioFraccion = new StringBuilder(".");
+
+        while (parteFraccionaria > 0) {
+            parteFraccionaria *= 2;
+            int bit = (int) parteFraccionaria;
+            binarioFraccion.append(bit);
+            parteFraccionaria -= bit;
+
+        }
+        return binarioEntero + (binarioFraccion.length() > 1 ? binarioFraccion.toString() : "");
+    }
+
+    public String conversionIEEEde64Bits(double num) {
+        signo = (num < 0) ? "1" : "0";
+        num = Math.abs(num);
+
+        double temp = num;
+        int iterador = 0;
+        while (temp > 2) {
+            temp /= 2;
+            iterador++;
+        }
+        while (temp < 1) {
+            temp *= 2;
+            iterador--;
+        }
+
+        int exponente = 1023 + iterador;
+        expBinary = String.format("%8s", decimalAbinario(exponente)).replace(' ', '0');
+
+        temp -= 1.0;
+        String binario = decimalAbinarioD(temp);
+
+        mantisa = binario.substring(2);
+        if (mantisa.length() > 52) {
+            mantisa = mantisa.substring(0, 52);
+        } else {
+            while (mantisa.length() < 52) {
+                mantisa += "0";
+            }
+        }
+        return signo + expBinary + mantisa;
+    }
+
+    private int binarioAdecimal(long binario) {
+        int decimal = 0;
+        int potencia = 0;
+        while (binario > 0) {
+            long num = binario % 10;
+            decimal += num * Math.pow(2, potencia);
+            potencia++;
+            binario /= 10;
+        }
+        return decimal;
+    }
+
+    public double conversionIEEEaDecimal(String num) {
+        if (num.length() == 32 || num.length() == 64) {
+            String signo = num.substring(0, 1);
+            String expBinario = (num.length() == 32) ? num.substring(1, 9) : num.substring(1, 12);
+            String mantisaBinaria = (num.length() == 32) ? num.substring(9) : num.substring(12);
+            int exponente = binarioAdecimal(Long.parseLong(expBinario)) - ((num.length() == 32) ? 127 : 1023);
+            double mantisaDecimal = convertirMantisa(mantisaBinaria);
+            double resultado = Math.pow(2, exponente) * mantisaDecimal;
+            return signo.equals("1") ? -resultado : resultado;
+        }
+        return Double.NaN;
+    }
+
+    private double convertirMantisa(String mantisaBinaria) {
+        double resultado = 1.0; // El 1 implícito en la mantisa normalizada
+
+        for (int i = 0; i < mantisaBinaria.length(); i++) {
+            if (mantisaBinaria.charAt(i) == '1') {
+                resultado += Math.pow(2, -(i + 1));
+            }
+        }
+        return resultado;
+    }
+
+}
---
+++ src/Vista/Test.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+import Modelo.Ieee;
+
+/**
+ *
+ * @author N
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Ieee ieee = new Ieee();
+
+        System.out.println("----------TEST DECIMAL A IEEE 754 DE 32 BITS ---------------");
+        System.out.println("Test Numero Grande positivo: " + ieee.conversionIEEEde32Bits(56.89));
+        System.out.println("Test Numero Grande positivo: " + ieee.conversionIEEEde32Bits(16.5));
+        System.out.println("Test Numero Grande Negativo: " + ieee.conversionIEEEde32Bits(-23.7));
+        System.out.println("Test Ceros: " + ieee.conversionIEEEde32Bits(0.0));
+        System.out.println("Test Numero Pequeño positivo: " + ieee.conversionIEEEde32Bits(0.654));
+        System.out.println("Test Numero Pequeño negativo: " + ieee.conversionIEEEde32Bits(-0.897));
+        
+        System.out.println("----------TEST DECIMAL A IEEE 754 DE 64 BITS ---------------");
+        System.out.println("Test Numero Grande positivo: " + ieee.conversionIEEEde64Bits(56.89));
+        System.out.println("Test Numero Grande positivo: " + ieee.conversionIEEEde64Bits(16.5));
+        System.out.println("Test Numero Grande Negativo: " + ieee.conversionIEEEde64Bits(-23.7));
+        //System.out.println("Test Ceros: " + ieee.conversionIEEEde64Bits(0.0));
+        System.out.println("Test Numero Pequeño positivo: " + ieee.conversionIEEEde64Bits(0.654));
+        System.out.println("Test Numero Pequeño negativo: " + ieee.conversionIEEEde64Bits(-0.897));
+        
+        
+        System.out.println("----------TEST IEEE 754 A DECIMAL   ---------------");
+        System.out.println("Test Numero IEEE 32 bits: " + ieee.conversionIEEEaDecimal("01000010011000111000111101011100"));
+        System.out.println("Test Numero IEEE 32 bits: " + ieee.conversionIEEEaDecimal("01000001100001000000000000000000"));
+        System.out.println("Test Numero IEEE 32 bits: " + ieee.conversionIEEEaDecimal("11000001101111011001100110011001"));
+        //System.out.println("Test Ceros: " + ieee.conversionIEEEde64Bits(0.0));
+        System.out.println("Test Numero IEEE 64 bits: " + ieee.conversionIEEEaDecimal("011111111100100111011011001000101101000011100101011000000100001"));
+        System.out.println("Test Numero IEEE 64 bits: " + ieee.conversionIEEEaDecimal("0100000001001100011100011110101110000101000111101011100001010010"));
+        
+        
+        System.out.println("----------SIGNO, EXPONENTE Y MANTISA   ---------------");
+        System.out.println("Test Numero Grande positivo: " + ieee.conversionIEEEde32Bits(56.89));
+        System.out.println("Signo: " + ieee.getSigno());
+        System.out.println("Exponente: " + ieee.getExpBinary());
+        System.out.println("Mantisa: " + ieee.getMantisa());
+        
+    }
+}
